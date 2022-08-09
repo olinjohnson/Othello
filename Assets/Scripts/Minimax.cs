e3b18d0cdeb15265d7a66208b0ec0e540d073dca
Olin Johnson <olinlj@icloud.com>
2022-08-09 00:19:18 -0400

    added evaluation function

--- Assets/Scripts/Minimax.cs
+++ Assets/Scripts/Minimax.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class Minimax
 {
+    /*
+     * The player (black), is trying to maximize the score,
+     * While the computer (white) tries to minimize it.
+     */
+
+    public Game game;
+
+    public Minimax()
+    {
+        game = new Game();
+    }
+
+
     int Evaluate(Board b)
     {
-        return 1;
+        // Count set bits
+        int w_count = 0;
+        int b_count = 0;
+        ulong black = b.black_pieces;
+        ulong white = b.white_pieces;
+
+        while (black > 0)
+        {
+            b_count += (int)(black & 1);
+            black >>= 1;
+        }
+        while (white > 0)
+        {
+            w_count += (int)(white & 1);
+            white >>= 1;
+        }
+
+        if (game.DetectEnding(b))
+        {
+            return (b_count - w_count) * 100;
+        }
+        else
+        {
+            return (b_count - w_count);
+        }
     }
 }
