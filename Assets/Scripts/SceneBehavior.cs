0f95c968e5c0ee6f6aeea14e58804e045f3fd183
Olin Johnson <olinlj@icloud.com>
2022-08-12 11:49:05 -0400

    Added options menu and main menu transitions, ui updates, build updates, upgraded unity version

--- Assets/Scripts/SceneBehavior.cs
+++ Assets/Scripts/SceneBehavior.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneBehavior : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
     public void LoadAIGame()
     {
         PlayerPrefs.AIOpponent = true;
@@ -14,6 +17,19 @@ public class SceneBehavior : MonoBehaviour
     {
         PlayerPrefs.AIOpponent = false;
         SceneManager.LoadScene("Main");
+    }
+    public void ToggleMainMenu()
+    {
+        if(mainMenu.activeInHierarchy)
+        {
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+        }
+        else
+        {
+            optionsMenu.SetActive(false);
+            mainMenu.SetActive(true);
+        }
     }
     public void BackToMenu()
     {
