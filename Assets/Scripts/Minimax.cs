18cfc588d1e9c68f7b4aecb45850336f94fe1470
Olin Johnson <olinlj@icloud.com>
2022-08-17 14:39:03 -0400

    Implemented negamax

--- Assets/Scripts/Minimax.cs
+++ Assets/Scripts/Minimax.cs
@@ -11,14 +11,14 @@ public class Minimax
     public uint nodes_searched;
     public int[] position_offsets = new int[]
     {
-        -15, -5, -5, -5, -5, -5, -5, -15,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -15, -5, -5, -5, -5, -5, -5, -15,
+        15, 5, 5, 5, 5, 5, 5, 15,
+        5, 0, 0, 0, 0, 0, 0, 5,
+        5, 0, 0, 0, 0, 0, 0, 5,
+        5, 0, 0, 0, 0, 0, 0, 5,
+        5, 0, 0, 0, 0, 0, 0, 5,
+        5, 0, 0, 0, 0, 0, 0, 5,
+        5, 0, 0, 0, 0, 0, 0, 5,
+        15, 5, 5, 5, 5, 5, 5, 15,
     };
 
     public Minimax(int d)
@@ -29,22 +29,21 @@ public class Minimax
 
     public int FindIdealMove(Board b)
     {
+        // White(AI) trying to maximize score, Black(Player) trying to minimize
         nodes_searched = 0;
         var watch = System.Diagnostics.Stopwatch.StartNew();
 
         ulong valid_moves = game.GetValidMoves(b);
-        int least = 10000;
-        int alpha = -10000;
-        int beta = 10000;
+        int most = -10000;
         int[] scores = new int[64];
-        Array.Fill(scores, 10000);
+        Array.Fill(scores, -10000);
 
         for (int i = 0; i < 64; i++)
         {
             if ((valid_moves & ((ulong)1 << i)) > 0)
             {
-                int val = Run(game.MakeMove(b, i), depth, 1, alpha, beta) + position_offsets[i];
-                least = Math.Min(least, val);
+                int val = -Run(game.MakeMove(b, i), depth - 1, -1) + position_offsets[i];
+                most = Math.Max(most, val);
                 scores[i] = val;
             }
         }
@@ -54,21 +53,21 @@ public class Minimax
         PlayerPrefs.nodesSearched = nodes_searched;
         PlayerPrefs.timeSearched = elapsed;
 
-        return Array.IndexOf(scores, least);
+        return Array.IndexOf(scores, most);
     }
 
-    public int Run(Board ghostMove, int d, int current_d, int alpha, int beta)
+    public int Run(Board ghostMove, int d, int color)
     {
         nodes_searched++;
         // Check for terminal state
         if(game.DetectEnding(ghostMove))
         {
-            return Evaluate(ghostMove, current_d) * 100;
+            return (Evaluate(ghostMove, d) + (d * color)) * 100;
         }
         // Check if max depth reached
-        else if(d == current_d)
+        else if(d == 0)
         {
-            return Evaluate(ghostMove, current_d);
+            return Evaluate(ghostMove, d) + (d * color);
         }
         else
         {
@@ -76,33 +75,17 @@ public class Minimax
             // If no valid moves
             if(valid_moves == 0)
             {
-                return Run(new Board(ghostMove.white_pieces, ghostMove.black_pieces, !ghostMove.turn), d, current_d + 1, alpha, beta);
+                return -Run(new Board(ghostMove.white_pieces, ghostMove.black_pieces, !ghostMove.turn), d - 1, -color);
             }
             else
             {
-                // Expand leaf nodes and return
-                int value = ghostMove.turn ? -10000 : 10000;
+                int value = -10000;
                 for (int i = 0; i < 64; i++)
                 {
                     if ((valid_moves & ((ulong)1 << i)) > 0)
                     {
-
-                        // Player turn - MAX 
-                        if (ghostMove.turn)
-                        {
-                            int child = Run(game.MakeMove(ghostMove, i), d, current_d + 1, alpha, beta) + Math.Abs(position_offsets[i]);
-                            value = Math.Max(value, child);
-                            alpha = Math.Max(alpha, value);
-                            if(alpha >= beta) { break; }
-                        }
-                        // AI turn - MIN
-                        else
-                        {
-                            int child = Run(game.MakeMove(ghostMove, i), d, current_d + 1, alpha, beta) + position_offsets[i];
-                            value = Math.Min(value, child);
-                            beta = Math.Min(beta, value);
-                            if (alpha >= beta) { break; }
-                        }
+                        int child = -Run(game.MakeMove(ghostMove, i), d - 1, -color) + position_offsets[i];
+                        value = Math.Max(value, child);
                     }
                 }
                 return value;
@@ -129,6 +112,6 @@ public class Minimax
             white >>= 1;
         }
 
-        return (b_count - w_count) + current_d;
+        return (w_count - b_count);
     }
 }
